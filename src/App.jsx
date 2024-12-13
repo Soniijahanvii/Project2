75839a62725c5191c4d391d992ef6afd74792685
Soniijahanvii <jahanvisoni@gmail.com>
2024-12-13 17:23:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <nav className="">
+        <h1 className="text-3xl font-bold font-serif mt-5 ml-7">Buffer</h1>
+        <a href="">
+          <ul className="flex justify-center gap-10  text-xl font-semibold">
+            <li></li>
+            <li>Tools</li>
+            <li>Channels</li>
+            <li>Pricing</li>
+            <li>Blog</li>
+          </ul>
+        </a>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
