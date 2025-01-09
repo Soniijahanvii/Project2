e9bc0fd766cefc5009bfcf68da5cf8a7dd451ab6
Soniijahanvii <jahanvisoni@gmail.com>
2025-01-10 00:47:57 +0530

    make responsive

--- src/component/Blog.jsx
+++ src/component/Blog.jsx
@@ -15,50 +15,53 @@ const Blog = () => {
       <div className="fixed gap-10 w-full z-50 bg-white">
         <Navbar />
       </div>
-      <div className="container mt-40 flex">
-        <div className="mt-10  w-full h-full ml-10  mr-2  flex justify-center">
+      <div className="mt-40 flex flex-col lg:flex-row items-center lg:items-start lg:justify-between px-4">
+        <div className="mt-10 w-full lg:w-1/2 flex justify-center">
           <img
             src={Blogimage}
             alt="Left Illustration"
-            className="w-auto h-auto object-cover"
+            className="max-w-full h-auto object-cover"
           />
         </div>
-        <div>
-          <div className="text-center lg:text-left text-xl mt-20">
+
+        <div className="mt-20 lg:mt-0 lg:mr-20 lg:w-1/2">
+          <div className="text-center lg:text-left text-xl mt-0 sm:mt-16">
             <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold">
-              How to Run Ads on TikTok: A Setp-by-Step Guide to TikTok
+              How to Run Ads on TikTok: A Step-by-Step Guide to TikTok
               Advertising
             </h1>
-            <p className="mt-16">
+            <p className="mt-8 sm:mt-12 lg:mt-16">
               This article takes you through everything you need to know to run
               TikTok ads and implement a solid advertising strategy.
             </p>
-            <div className="flex gap-24">
-              <p className="mt-10 text-sm">Jan 9, 2025</p>
-              <p className="mt-10 text-sm"> 16 min read</p>
-              <p className="mt-10 text-sm">TikTok</p>
+            <div className="flex flex-wrap justify-center lg:justify-start gap-8 sm:gap-12 lg:gap-24 mt-8">
+              <p className="text-sm">Jan 9, 2025</p>
+              <p className="text-sm">16 min read</p>
+              <p className="text-sm">TikTok</p>
             </div>
           </div>
         </div>
       </div>
-      <div className="mt-20">
+
+      <div className="mt-20 sm:ml-10 sm:mr-10">
         <h1 className="text-xl font-bold text-center">Latest Updates</h1>
-        <div className="flex-grow border-t border-gray-400 ml-44 mr-44"></div>
-        <div className="flex mt-5 gap-5 ml-16 mr-16">
-          <div className=" border shadow-xl">
-            <div className=" p-10">
+        <div className="flex-grow border-t border-gray-400 mx-10 lg:mx-44"></div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mt-5 px-5 sm:px-16 lg:px-0">
+          <div className="border shadow-xl">
+            <div className="p-5">
               <img
                 src={Blog1}
                 alt="Left Illustration"
-                className="w-96 h-60 object-cover"
+                className="w-full h-60 object-cover"
               />
             </div>
             <div className="p-5">
-              <div className="flex gap-28">
-                <p className="text-lg text-blue-500 mt-4 ml-1">
+              <div className="flex justify-between items-center">
+                <p className="text-lg text-blue-500 mt-4">
                   Social Media Marketing
                 </p>
-                <p className="text-sm  mt-4 ml-16">Jan 8, 2025</p>
+                <p className="text-sm mt-4">Jan 8, 2025</p>
               </div>
               <div className="mt-2">
                 <h1 className="text-2xl font-bold underline text-black">
@@ -73,20 +76,21 @@ const Blog = () => {
               </div>
             </div>
           </div>
+
           <div className="border shadow-xl">
-            <div className="p-10">
+            <div className="p-5">
               <img
                 src={Blog2}
                 alt="Left Illustration"
-                className="w-96 h-60 object-cover"
+                className="w-full h-60 object-cover"
               />
             </div>
             <div className="p-5">
-              <div className="flex gap-28">
-                <p className="text-lg text-blue-500 mt-4 ml-1">
+              <div className="flex justify-between items-center">
+                <p className="text-lg text-blue-500 mt-4">
                   Social Media Marketing
                 </p>
-                <p className="text-sm mt-4 ml-16">Jan 8, 2025</p>
+                <p className="text-sm mt-4">Jan 8, 2025</p>
               </div>
               <div className="mt-2">
                 <h1 className="text-2xl font-bold underline text-black">
@@ -102,19 +106,19 @@ const Blog = () => {
             </div>
           </div>
           <div className="border shadow-xl">
-            <div className="p-10">
+            <div className="p-5">
               <img
                 src={Blog3}
                 alt="Left Illustration"
-                className="w-96 h-60 object-cover"
+                className="w-full h-60 object-cover"
               />
             </div>
             <div className="p-5">
-              <div className="flex gap-28">
-                <p className="text-lg text-blue-500 mt-4 ml-1">
+              <div className="flex justify-between items-center">
+                <p className="text-lg text-blue-500 mt-4">
                   Social Media Marketing
                 </p>
-                <p className="text-sm mt-4 ml-16">Jan 8, 2025</p>
+                <p className="text-sm mt-4">Jan 8, 2025</p>
               </div>
               <div className="mt-2">
                 <h1 className="text-2xl font-bold underline text-black">
@@ -130,21 +134,21 @@ const Blog = () => {
             </div>
           </div>
         </div>
-        <div className="flex mt-5 gap-5 ml-16 mr-16">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mt-5 px-5 sm:px-16 lg:px-0">
           <div className="border shadow-xl">
-            <div className="p-10">
+            <div className="p-5">
               <img
                 src={Blog4}
                 alt="Left Illustration"
-                className="w-96 h-60 object-cover"
+                className="w-full h-60 object-cover"
               />
             </div>
             <div className="p-5">
-              <div className="flex gap-28">
-                <p className="text-lg text-blue-500 mt-4 ml-1">
+              <div className="flex justify-between items-center">
+                <p className="text-lg text-blue-500 mt-4">
                   Social Media Marketing
                 </p>
-                <p className="text-sm mt-4 ml-16">Jan 8, 2025</p>
+                <p className="text-sm mt-4">Jan 8, 2025</p>
               </div>
               <div className="mt-2">
                 <h1 className="text-2xl font-bold underline text-black">
@@ -159,20 +163,21 @@ const Blog = () => {
               </div>
             </div>
           </div>
+
           <div className="border shadow-xl">
-            <div className="p-10">
+            <div className="p-5">
               <img
                 src={Blog5}
                 alt="Left Illustration"
-                className="w-96 h-60 object-cover"
+                className="w-full h-60 object-cover"
               />
             </div>
             <div className="p-5">
-              <div className="flex gap-28">
-                <p className="text-lg text-blue-500 mt-4 ml-1">
+              <div className="flex justify-between items-center">
+                <p className="text-lg text-blue-500 mt-4">
                   Social Media Marketing
                 </p>
-                <p className="text-sm mt-4 ml-16">Jan 8, 2025</p>
+                <p className="text-sm mt-4">Jan 8, 2025</p>
               </div>
               <div className="mt-2">
                 <h1 className="text-2xl font-bold underline text-black">
@@ -188,19 +193,19 @@ const Blog = () => {
             </div>
           </div>
           <div className="border shadow-xl">
-            <div className="p-10">
+            <div className="p-5">
               <img
                 src={Blog6}
                 alt="Left Illustration"
-                className="w-96 h-60 object-cover"
+                className="w-full h-60 object-cover"
               />
             </div>
             <div className="p-5">
-              <div className="flex gap-28">
-                <p className="text-lg text-blue-500 mt-4 ml-1">
+              <div className="flex justify-between items-center">
+                <p className="text-lg text-blue-500 mt-4">
                   Social Media Marketing
                 </p>
-                <p className="text-sm mt-4 ml-16">Jan 8, 2025</p>
+                <p className="text-sm mt-4">Jan 8, 2025</p>
               </div>
               <div className="mt-2">
                 <h1 className="text-2xl font-bold underline text-black">
@@ -217,6 +222,7 @@ const Blog = () => {
           </div>
         </div>
       </div>
+
       <div className="mt-20">
         <Footer />
       </div>
